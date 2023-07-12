811970523961167a67a8a935ac3d42bf7baa8bf0
Isgandarov Khayyam <[khayyamni@code.edu.az]>
2023-07-12 18:00:35 +0400

    TasksCsharp

---
+++ CSharpCode/CSharpCode/Program.cs
@@ -0,0 +1,85 @@
+﻿//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+//    int n=21;
+//if (n % 3 == 0 && n % 7 == 0)
+//{
+
+//    Console.WriteLine("Bolunur");
+
+//}
+//else
+//{ Console.WriteLine("Bolunmur"); }
+
+
+
+//2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın.
+
+
+//int n = 12;
+//int m = 24;
+//int sum = 0;
+
+//if (n % 2 == 0 && m % 2 == 0)
+
+//{
+
+//    sum = n + m;
+
+
+//}
+
+
+//{ Console.WriteLine(sum); }
+
+//3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin.
+//int n = 10;
+//int m = 26;
+//int sum = 0;
+
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+
+//4) Verilmish arrayin icindeki tek ededlerin cemini tapin.
+
+//int[] nums = { 1, 5, 13, 7, 4 };
+//int sum = 0;
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] % 2 == 1)
+//    {
+//        sum += nums[i];
+
+
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+//5) Verilmish arrayin icindeki cut ededlerin sayini tapin.
+
+
+
+//int[] numbers = { 3, 1, 8, 14, 31 };
+//int count = 0;
+//for (int i = 0; i < numbers.Length; i++)
+
+//{
+
+//    if (numbers[i] % 2 == 0)
+
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
